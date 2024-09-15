3f7bece575bb34ae41d21e1cc1a17288fea6d381
human-delegation <316211300@qq.com>
2024-09-15 23:27:09 +0800

    update

--- script/2-Regression.py
+++ script/2-Regression.py
@@ -1,40 +1,50 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import numpy as np
+
 from sklearn.neural_network import MLPRegressor
+
 
 def main():
     # Configurations
-    HOUR = 8
-    PERIOD = 1
+    HOUR = 9
+    PERIOD = 2
     WEEKDAY = True
     HISTORICAL_WINDOW = 5
     TEST_HORIZON = 10
 
-    df_data = pd.read_csv('../data/2-DataWeather/Station_Station72_whole_life.csv')
+    df_data_focal = pd.read_csv('../data/2-DataWeather/Station_Station72_whole_life.csv')
+    df_data_nearby = pd.read_csv('../data/2-DataWeather/Station_Station480_whole_life.csv')
 
     if WEEKDAY:
-        df_data_sub = df_data[(df_data['hour'] == HOUR) & (df_data['period'] == PERIOD) & (df_data['weekday'] <= 4)]
+        df_data_focal_sub = df_data_focal[(df_data_focal['hour'] == HOUR) & (df_data_focal['period'] == PERIOD) & (df_data_focal['weekday'] <= 4)].reset_index().drop(['index'], axis=1)
+        df_data_nearby_sub = df_data_nearby[(df_data_nearby['hour'] == HOUR) & (df_data_nearby['period'] == PERIOD) & (df_data_nearby['weekday'] <= 4)].reset_index().drop(['index'], axis=1)
+
 
     else:
-        df_data_sub = df_data[(df_data['hour'] == HOUR) & (df_data['period'] == PERIOD) & (df_data['weekday'] > 4)]
+        df_data_focal_sub = df_data_focal[(df_data_focal['hour'] == HOUR) & (df_data_focal['period'] == PERIOD) & (df_data_focal['weekday'] > 4)].reset_index().drop(['index'], axis=1)
+        df_data_nearby_sub = df_data_nearby[(df_data_nearby['hour'] == HOUR) & (df_data_nearby['period'] == PERIOD) & (df_data_nearby['weekday'] > 4)].reset_index().drop(['index'], axis=1)
 
     # Adding lag features
     for i in range(1, HISTORICAL_WINDOW+1):
-        df_data_sub[f'pickup_lag_{i}'] = df_data_sub['pickup'].shift(i)
+        df_data_focal_sub[f'pickup_lag_{i}'] = df_data_focal_sub['pickup'].shift(i)
+        df_data_focal_sub[f'nearby_pickup_lag_{i}'] = df_data_nearby_sub['pickup'].shift(i)
 
     # Delete the first historical_window rows of data
-    df_data_sub = df_data_sub.iloc[HISTORICAL_WINDOW:]
+    df_data_focal_sub = df_data_focal_sub.iloc[HISTORICAL_WINDOW:]
 
     # Split the training and test dataset
     feas_cols = ['is_empty_(0, 0)', 'is_empty_(0, 1)', 'is_empty_(1, 0)', 'is_empty_(1, 1)', 'temperature', 'humidity',
-                 'visibility', 'windspeed', 'condition'] + [f'pickup_lag_{i}' for i in range(1, HISTORICAL_WINDOW+1)]
+                 'visibility', 'windspeed', 'condition'] \
+                + [f'pickup_lag_{i}' for i in range(1, HISTORICAL_WINDOW+1)] \
+                + [f'nearby_pickup_lag_{i}' for i in range(1, HISTORICAL_WINDOW+1)]
 
-    df_train_X = df_data_sub[feas_cols][:-TEST_HORIZON]
-    df_train_y = df_data_sub['pickup'][:-TEST_HORIZON]
+    df_train_X = df_data_focal_sub[feas_cols][:-TEST_HORIZON]
+    df_train_y = df_data_focal_sub['pickup'][:-TEST_HORIZON]
 
-    df_test_X = df_data_sub[feas_cols][-TEST_HORIZON:]
-    df_test_y = df_data_sub['pickup'][-TEST_HORIZON:]
+    df_test_X = df_data_focal_sub[feas_cols][-TEST_HORIZON:]
+    df_test_y = df_data_focal_sub['pickup'][-TEST_HORIZON:]
 
     # Iterated retraining & predicting for the test dataset
     test_predictions = []
